8ec2bd56bbc21da7a8853bb35abbbd7a6a4fe1f5
gamevuihoa <154033211+gamevuihoa@users.noreply.github.com>
2024-01-22 19:14:07 +0900

    Add files via upload

---
+++ GoalWindow.java
@@ -0,0 +1,24 @@
+import java.util.Optional;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Alert.AlertType;
+
+public class GoalWindow {
+    protected static boolean onOpen() {
+        Alert alrt = new Alert(AlertType.CONFIRMATION);
+        alrt.setTitle(null);
+        alrt.setHeaderText("Game Clear!");
+        alrt.setContentText(/*"Score: " + MapGameController.chara.getScore + "\n" + */"Start new game?");
+        Optional<ButtonType> result = alrt.showAndWait();
+        if (result.get() == ButtonType.OK) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
